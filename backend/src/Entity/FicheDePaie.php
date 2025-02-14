dbe1ae90df555f3d88dbff5b7afb37d7914cb20d
Bastien <record.bastien@gmail.com>
2025-02-14 11:51:17 +0100

    feat(entity): Create all Entity

---
+++ backend/src/Entity/FicheDePaie.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FicheDePaieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FicheDePaieRepository::class)]
+class FicheDePaie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'fichesDePaie')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Coach $coach = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $periode = null;
+
+    #[ORM\Column]
+    private ?int $total_heures = null;
+
+    #[ORM\Column]
+    private ?float $montant_total = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCoach(): ?Coach
+    {
+        return $this->coach;
+    }
+
+    public function setCoach(?Coach $coach): static
+    {
+        $this->coach = $coach;
+
+        return $this;
+    }
+
+    public function getPeriode(): ?string
+    {
+        return $this->periode;
+    }
+
+    public function setPeriode(string $periode): static
+    {
+        $this->periode = $periode;
+
+        return $this;
+    }
+
+    public function getTotalHeures(): ?int
+    {
+        return $this->total_heures;
+    }
+
+    public function setTotalHeures(int $total_heures): static
+    {
+        $this->total_heures = $total_heures;
+
+        return $this;
+    }
+
+    public function getMontantTotal(): ?float
+    {
+        return $this->montant_total;
+    }
+
+    public function setMontantTotal(float $montant_total): static
+    {
+        $this->montant_total = $montant_total;
+
+        return $this;
+    }
+}
